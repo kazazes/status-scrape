13bf66f1faad5c9372dcc58d5e9b61643e0487ef
Peter Kazazes <peter@peterk.co>
2018-09-30 14:44:41 -0700

    stub parsing implementation

--- packages/functions/src/strategies/statuspageStrategy.ts
+++ packages/functions/src/strategies/statuspageStrategy.ts
@@ -1,10 +1,31 @@
-import { StatusScrapeTargetNode } from "../prisma";
+import cheerio from "cheerio";
+import {
+  StatusScrapeResultCreateInput,
+  StatusScrapeTargetNode
+} from "../prisma";
 import { ScraperStrategy } from "./strategy";
 
-// tslint:disable:no-console
 export class StatuspageStrategy extends ScraperStrategy {
   constructor(target: StatusScrapeTargetNode) {
     super(target);
     this.strategy = "STATUSPAGE_IO";
+  }
+
+  public async parse() {
+    const $ = cheerio.load(this.dom);
+    const components = $("body div.component-container");
+    const categories = components.map(component => {
+      const title = $(component)
+        .find("span.name")
+        .first()
+        .text()
+        .trim();
+
+      const items = $(component).find("component-inner-container");
+      return [title, items];
+    });
+    categories.empty();
+    const inputs: StatusScrapeResultCreateInput[] = [];
+    return inputs;
   }
 }
