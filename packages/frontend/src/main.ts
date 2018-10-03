0cb34384097f8512c115ab8577d3b2a73ae38aae
Peter Kazazes <peter@peterk.co>
2018-10-02 20:42:47 -0700

    apollo wip

---
+++ packages/frontend/src/main.ts
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import './vendor/bootstrap/bootstrap.scss';
+import 'mdbvue/build/scss/mdb.scss';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import { createProvider } from './vue-apollo'
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  apolloProvider: createProvider(),
+  render: (h) => h(App)
+}).$mount('#app');
---
+++ packages/frontend/vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  pluginOptions: {
+    apollo: {
+      typescript: true,
+    },
+  },
+};
