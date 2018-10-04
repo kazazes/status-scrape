2b04e473ba3de030718449163d2f4f1c4652e112
Peter Kazazes <peter@peterk.co>
2018-10-03 18:13:31 -0700

    hotfix postcss config path

--- packages/frontend/vue.config.js
+++ packages/frontend/vue.config.js
@@ -1,34 +1,34 @@
 module.exports = {
   pluginOptions: {
     apollo: {
-      typescript: true
-    }
+      typescript: true,
+    },
   },
   css: {
     loaderOptions: {
       postcss: {
         config: {
-          path: "/Users/pk/repositories/status-scrape/packages/frontend"
-        }
-      }
+          path: __dirname,
+        },
+      },
     },
-    sourceMap: true
+    sourceMap: true,
   },
-  chainWebpack: config => {
-    config.plugin("fork-ts-checker").tap(args => {
+  chainWebpack: (config) => {
+    config.plugin('fork-ts-checker').tap((args) => {
       const a = args;
       a.tslint = `${__dirname}/tslint.json`;
       return a;
     });
 
     config.module
-      .rule("ts")
-      .use("ts-loader")
-      .loader("ts-loader")
-      .tap(options => {
+      .rule('ts')
+      .use('ts-loader')
+      .loader('ts-loader')
+      .tap((options) => {
         const o = options;
         o.configFile = `${__dirname}/tsconfig.json`;
         return o;
       });
-  }
+  },
 };
